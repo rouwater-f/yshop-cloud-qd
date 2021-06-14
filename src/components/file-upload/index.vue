839928ee97f28b82d0962a5242098485ac423802
Bug <18026623439@163.com>
2021-06-15 00:18:17 +0800

    文件上传无权限

--- src/components/file-upload/index.vue
+++ src/components/file-upload/index.vue
@@ -29,7 +29,7 @@ export default {
     return {
       resourcesUrl: '',
       headers: {
-        'Authorization': getToken()
+        'Authorization': 'Bearer ' + getToken()
       }
     }
   },
